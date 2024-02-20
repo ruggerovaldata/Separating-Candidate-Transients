88e95b628c335aa263cb49995f118b98b1cd7517
Antonia Rowlinson <antonia@antonias-air.home>
2024-02-20 10:56:01 +0100

    Reimplementing the 'Best' method

--- FindingCandidateTransients.py
+++ FindingCandidateTransients.py
@@ -64,6 +64,7 @@ data['distsV'] = data.apply(lambda row: func.Params_distance(row.logV,func.Linea
 
 
 #Creating a matrix necessary to calculate the Gaussian distribution of the data.
+print('\n WHOLE DATASET: \n')
 
 ndim = 2
 data_graph = np.vstack([data.distsEta,data.distsV])
@@ -122,3 +123,67 @@ else:
     print(outliers)
     outliers.to_csv('Outliers.csv', index=False)
 
+
+
+    
+print('\n DATASET ABOVE THE LINE: \n')
+
+ndim = 2
+
+dataBest = data.loc[(data['distsEta'] > 0) & (data['distsV'] > 0)]
+
+data_graph = np.vstack([dataBest.distsEta,dataBest.distsV])
+data_graph = data_graph.T
+
+mean_deta = np.mean(dataBest.distsEta)
+mean_dV = np.mean(dataBest.distsV)
+mu = [mean_deta,mean_dV]
+
+cov_matrix = np.cov(dataBest.distsEta,dataBest.distsV)
+likelihood = spstat.multivariate_normal.pdf(data_graph,[mean_deta,mean_dV],cov_matrix)
+
+outliers_prob = func.Probability(data_graph,mu,cov_matrix) #Calculating the probability for every parameter of being associated to an "inlier" source
+
+dataBest['probability'] = 100.-outliers_prob
+temp = dataBest.sort_values('probability')
+dataBest.to_csv('WholedatasetoutputBest.csv', index=False)
+
+figure, axes = myplt.MyCorner(dataBest.distsEta,dataBest.distsV,dataBest.probability/100.,'CornerPlotBest') #Printing the corner plot both with the likelihood and without the likelihood
+
+chi2 = spstat.chi2.ppf([p],2)[0]
+
+# finding the outliers with probabilities >99% and with positive distances abovve the trend line
+inliersBest = dataBest.loc[ (dataBest['probability'] <= p*100.) ]
+outliersBest = dataBest.loc[ (dataBest['probability'] > p*100.) ]
+
+# Plotting
+fig,(ax1,ax2) = plt.subplots(2,1,figsize=(14,14))
+
+ax1.scatter(outliersBest.logFlux,outliersBest.logEta,color='red',label='Outliers')
+ax1.scatter(inliersBest.logFlux,inliersBest.logEta,color='blue',label='Inliers')
+ax2.scatter(outliersBest.logFlux,outliersBest.logV,color='red',label='Outliers')
+ax2.scatter(inliersBest.logFlux,inliersBest.logV,color='blue',label = 'Inliers')
+
+ax1.set_ylabel(r'$log_{10}(\eta_{\nu}$)',fontsize=30)
+ax2.legend(fontsize=15,markerscale=1.5)
+ax1.legend(fontsize=15,markerscale=1.5)
+ax2.set_ylabel(r'$log_{10}(V_{\nu}$)',fontsize=30)
+ax2.set_xlabel(r'$log_{10}(Flux) (Jy)$',fontsize=30)
+ax1.tick_params(labelsize=15)
+ax2.tick_params(labelsize=15)
+plt.savefig('EtavsVscatterinoutBest')
+
+figure,ax = myplt.OutInPlot(np.array([outliersBest.distsEta, outliersBest.distsV]).T,np.array([inliersBest.distsEta,inliersBest.distsV]).T,'OutIn_UnstableBest')
+EtaVsVout, axveta = myplt.OutInPlot(np.array([outliersBest.logEta, outliersBest.logV]).T,np.array([inliersBest.logEta,inliersBest.logV]).T,'OutInEtavsVBest')
+axveta.set_xlabel(r'$log_{10}(\eta_{\nu})$',fontsize=30)
+axveta.set_ylabel(r'$log_{10}(V_{\nu})$',fontsize=30)
+plt.savefig('OutInEtavsVBest')
+
+# Outputting variable candidates
+print('Number of outliers : ', len(outliersBest))
+
+if len(outliersBest) == 0:
+    print('No candidate variable sources found.')
+else:
+    print(outliersBest)
+    outliersBest.to_csv('OutliersBest.csv', index=False)
