2524dba72978d5f53b7973fcb2f332443f7048d2
ruggerovaldata <ruggerovaldata@gmail.com>
2024-08-20 10:56:03 +0200

    dblogin.py

--- dblogin.py
+++ dblogin.py
@@ -1,6 +1,13 @@
 engine = 'postgresql'
+<<<<<<< Updated upstream
 host = ''
 port = 5432
 user = ''
 password = ''
+=======
+host = ''
+port = 5432
+user = ''
+password = ''
+>>>>>>> Stashed changes
 
